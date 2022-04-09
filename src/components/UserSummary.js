06fa531449e9da16ebb9fe9049e4c47235bc9427
Charlton_Rosero <charltonprosero@gmail.com>
2022-04-08 21:43:36 -0400

    Toggle is work in Progress

--- src/components/UserSummary.js
+++ src/components/UserSummary.js
@@ -4,14 +4,21 @@ function UserSummary(props){
     const data = props.currentUser;
     const [moreInfo, setMoreInfo]= useState(false)
 
-    // EHH I'll DO THIS TOMORROW
-
-
+    const showMore = async (event) =>{
+        setMoreInfo(true)
+    }
+ 
+    const showLess= async (event) =>{
+        setMoreInfo(false)
+    }
+    console.log(moreInfo)
     if(data){
+
         return(
             <div>
                 <p>{data.name.title}. {data.name.first} {data.name.last}</p> 
                 <p>{data.email}</p>
+               <button onClick={(event) => showMore(event)}>More Info</button> 
             </div>
 
 
